91b5204f338ce2976060c3fc85f23ea078541f3a
juan pico <juan.pico-m@mail.escuelaing.edu.co>
2022-09-18 22:45:09 -0500

    Terminado

---
+++ README.md
@@ -0,0 +1,21 @@
+# Calculadora de complejos
+
+## Integragrantes 
+Juan Andrés PIco 
+
+En este laboratorio se busco hacer una calculadora de complejos con las siguientes operaciones:
+
+- Los experimentos de la canicas con coeficiente booleanos
+- Experimentos de las múltiples rendijas clásico probabilístico, con más de dos rendijas.
+- Experimento de las múltiples rendijas cuántico.
+- Cree una función para graficar con un diagrama de barras que muestre las probabilidades de un vector de estados. La imagen se debe poder guardar en el computador con un formato de imagen.
+# Ejecución del programa
+Para un correcto uso del programa se creo un nuevo py para no tener todo el codigo junto llamado de lo clasicoaCuantico teniendo diferentes funciones dependiendo de que operación se desee realizar, todo esta puesto en el main con pruebas,
+![](/img/func.png)
+
+todas las funciones en main tienen su respectiva prueba con los valores y las matrices , para la funcion de graficar se importo numpy y  matplotlib.pyplot
+
+![](/img/main.png)
+
+Para al final tener el resultado puesto a calculado.
+![](/img/output.png)
--- clasicoaCuantico.py
+++ clasicoaCuantico.py
@@ -1,27 +1,145 @@
 from numerosComplejos import *
-
+import matplotlib.pyplot as plot
+import numpy as np
 def main():
-    matriz = [[[-5,0],[3,0],[4,0],[-1,0]],[[7,0],[5,0],[2,0],[4,0]],[[2,0],[-1,0],[-6,0],[0,0]]]
-    vector = [[4,0],[2,0],[-6,0]]
-    print("accion " + str(accionEntreMatrizyVector(matriz,vector)))
+    booleanMatrix = [[False, False, False, False, False, False], [True, False, False, False, False, True],
+                     [True, False, False, False, False, False], [
+        False, False, True, False, False, False],
+        [False, False, False, True, False, False], [False, False, False, False, True, False]]
 
-def accionEntreMatrizyVector(matrix, vector):
+    vectIni = [True, False, False, False, False, False]
+    print("Experimento canicas " + str(experimentoCanicas(1, booleanMatrix[:], vectIni[:])))
+    
+    matrix = [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0.3333333333333333, 0], [0, 0], [0, 0], [
+                  0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0.3333333333333333, 0], [0, 0], [0, 0], [
+                  0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0.3333333333333333, 0], [0, 0], [0, 0], [
+                  0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0, 0], [0.5, 0], [0, 0], [0, 0], [
+                  0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0, 0], [0.5, 0], [0.5, 0], [0, 0],
+               [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0, 0], [0, 0], [0.5, 0], [0.5, 0],
+               [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0, 0], [0, 0], [0, 0], [0.5, 0], [0, 0], [0, 0], [0, 0], [0, 0]]]
+
+    vectIni = [[1, 0], [0, 0], [0, 0], [
+        0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    
+    print("Multiple rendija clasico  " +
+          str(multipleRendijaClasico(matrix[:], vectIni[:], 1)))
+    
+    matrix = [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0.7071067811865475, 0], [0, 0], [0, 0], [
+                  0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0.7071067811865475, 0], [0, 0], [0, 0], [
+                  0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0, 0], [-0.4082482904638631, 0.4082482904638631],
+               [0, 0], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+              [[0, 0], [-0.4082482904638631, -0.4082482904638631],
+               [0, 0], [0, 0], [1, 0], [0, 0], [0, 0], [0, 0]],
+              [[0, 0], [0.4082482904638631, -0.4082482904638631], [-0.4082482904638631,
+                                                                   0.4082482904638631], [0, 0], [0, 0], [1, 0], [0, 0], [0, 0]],
+              [[0, 0], [0, 0], [-0.4082482904638631, -0.4082482904638631],
+               [0, 0], [0, 0], [0, 0], [1, 0], [0, 0]],
+              [[0, 0], [0, 0], [0.4082482904638631, -0.4082482904638631], [0, 0], [0, 0], [0, 0], [0, 0], [1, 0]]]
+
+    vectIni = [[1, 0], [0, 0], [0, 0], [
+        0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    
+    print("Multiple rendija cuantico  " + str(multipleRendijaCuantico(matrix[:], vectIni[:], 1)))
+    
+    Matriz_Doble_Rendija = [
+        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+        [[1 / math.sqrt(2), 0], [0, 0], [0, 0], [0, 0],
+            [0, 0], [0, 0], [0, 0], [0, 0]],
+        [[1 / math.sqrt(2), 0], [0, 0], [0, 0], [0, 0],
+            [0, 0], [0, 0], [0, 0], [0, 0]],
+        [[0, 0], [-1 / math.sqrt(6), 1 / math.sqrt(6)],
+            [0, 0], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+        [[0, 0], [-1 / math.sqrt(6), -1 / math.sqrt(6)],
+            [0, 0], [0, 0], [1, 0], [0, 0], [0, 0], [0, 0]],
+        [[0, 0], [1 / math.sqrt(6), -1 / math.sqrt(6)], [-1 / math.sqrt(6),
+                                                         1 / math.sqrt(6)], [0, 0], [0, 0], [1, 0], [0, 0], [0, 0]],
+        [[0, 0], [0, 0], [-1 / math.sqrt(6), -1 / math.sqrt(6)],
+            [0, 0], [0, 0], [0, 0], [1, 0], [0, 0]],
+        [[0, 0], [0, 0], [1 / math.sqrt(6), -1 / math.sqrt(6)], [0, 0], [0, 0], [0, 0], [0, 0], [1, 0]]]
+
+    Estado_Inicial = [[1, 0], [0, 0], [0, 0], [
+        0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+
+    answ = accionMatrizVector(multipleRendijaCuantico(Matriz_Doble_Rendija[:],
+                                                              Estado_Inicial[:], 2),  Estado_Inicial)
+    graphProbabilitiesVector(answ)
+    
+
+def accionBooleanentreMatrizVector(matrix, vector):
     row, col = len(matrix), len(matrix[0])
     length = len(vector)
 
     if (col == length):
-        answ = [[0, 0] for x in range(row)]
+        answ = [False for c in range(row)]
 
         for i in range(row):
+            And = True
+
             for j in range(col):
-                mult = multiplicarComplejos(matrix[i][j], vector[j])
-                answ[i] = sumarComplejos(answ[i], mult)
+                And = matrix[i][j] and vector[j]
+                answ[i] = answ[i] or And
 
         return answ
     print("Las dimensiones de las matrices, no son los adecuados para su multiplicacion")
+    print("Las dimensiones de las matrices, no son los adecuados para su multiplicacion")
 
-    
-    
+def experimentoCanicas( clicks ,booleanMatrix, vectIni  ):
+    if ( clicks >= 0 and type( clicks ) is int ):
+        for c in range( clicks ):
+            vectIni = accionBooleanentreMatrizVector(booleanMatrix, vectIni)
+        return vectIni
+
+
+def multipleRendijaClasico(matrix, vectIni, clicks):
+    if (clicks >= 0) and (type(clicks) is int):
+        for x in range(clicks):
+            vectIni = accionMatrizVector(matrix, vectIni)
+        return vectIni
+    return -1
+
+
+def multipleRendijaCuantico(matrix, vectIni, clicks):
+    if ( clicks  >= 0 ) and ( type( clicks ) is int ):
+        length  = len( vectIni )
+        copyMatrix = matrix[:]
+        
+        for x in range(clicks  ):
+            matrix = multiplicacionDeMarticesIguales( matrix, copyMatrix)
+
+        return matrizFinal(matrix)
+    return -1
+
+
+def graphProbabilitiesVector( vector  ):
+    x = np.array([x for x in range(len(vector))])
+    y = np.array([round(vector[x][0]*100, 2) for x in range(len(vector))])
+
+    plot.bar( x,y , color ='g', align='center')
+    plot.title('Probabilidades vector')
+    plot.show()
+  
+  
+
+def matrizFinal(matrix):
+    row, column = len(matrix), len(matrix[0])
+    for i in range(row):
+        nRow = []
+        for j in range(column):
+            nRow.append([(moduloComplejos(matrix[i][j])**2), 0])
+
+        matrix[i] = nRow
+    return matrix
+
 
     
     
